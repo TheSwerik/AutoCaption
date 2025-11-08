1b4c110828b26ee6b9f3d755e745671a6efebb3e
TheSwerik <derswerik@gmail.com>
2025-11-08 15:53:05 +0100

    add App

---
+++ Desktop/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,6 @@
+﻿namespace Desktop.ViewModels;
+
+public class MainWindowViewModel : ViewModelBase
+{
+    public string Greeting { get; } = "Welcome to Avalonia!";
+}
---
+++ Desktop/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace Desktop.ViewModels;
+
+public abstract class ViewModelBase : ObservableObject
+{
+}
