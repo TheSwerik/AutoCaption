7ccb400f30c5c8ab3784e5482cecbfc189e3b1f8
TheSwerik <derswerik@gmail.com>
2025-11-15 16:25:24 +0100

    refactor Whisper-Calling

---
+++ Desktop/Services/WhisperService.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace Desktop.Services;
+
+public static class WhisperService
+{
+    public static event EventHandler OnOutput;
+    public static event EventHandler OnError;
+
+    public static async Task Start(params WhisperSettings[] settings)
+    {
+        foreach (var setting in settings) await Start(setting);
+    }
+
+    public static async Task Start(WhisperSettings settings)
+    {
+        List<string> arguments =
+        [
+            "whisper",
+            $"\"{settings.FilePath}\"",
+            $"--device {(ConfigService.Config.UseGpu ? "cuda" : "cpu")}",
+            $"-o {settings.OutputLocation}",
+            $"--output_format {ConfigService.Config.OutputFormat.ToString().ToLowerInvariant()}",
+            $"--model {ConfigService.Config.Model.ToString().ToLowerInvariant()}",
+            $"--language {settings.Language}"
+        ];
+
+        var info = new ProcessStartInfo
+        {
+            FileName = ConfigService.Config.PythonLocation,
+            Arguments = $"-m {string.Join(' ', arguments)}",
+            RedirectStandardOutput = true,
+            RedirectStandardError = true,
+            UseShellExecute = false,
+            CreateNoWindow = true
+        };
+
+        Console.WriteLine(info.Arguments);
+
+        var proc = new Process();
+        proc.StartInfo = info;
+
+        proc.OutputDataReceived += OutputReceived;
+        proc.ErrorDataReceived += ErrorReceived;
+
+        proc.Start();
+        proc.BeginOutputReadLine();
+        proc.BeginErrorReadLine();
+
+        await proc.WaitForExitAsync();
+
+        if (proc.ExitCode != 0) throw new Exception("Python Exitcode: " + proc.ExitCode);
+    }
+
+    private static void OutputReceived(object sender, DataReceivedEventArgs e)
+    {
+        Console.WriteLine(sender + " " + e.Data);
+        OnOutput.Invoke(sender, e);
+    }
+
+    private static void ErrorReceived(object sender, DataReceivedEventArgs e)
+    {
+        Console.WriteLine("ERROR: " + sender + " " + e.Data);
+        OnOutput.Invoke(sender, e);
+    }
+}
+
+public record WhisperSettings(string FilePath, string OutputLocation, string Language);
--- Desktop/Views/MainWindow.axaml.cs
+++ Desktop/Views/MainWindow.axaml.cs
@@ -1,7 +1,5 @@
-using System;
-using System.Collections.Generic;
-using System.Diagnostics;
 using System.Globalization;
+using System.Linq;
 using Avalonia.Controls;
 using Avalonia.Interactivity;
 using Avalonia.Platform.Storage;
@@ -31,46 +29,8 @@ public partial class MainWindow : Window
             Title = "Input Files"
         };
         var files = await StorageProvider.OpenFilePickerAsync(opt);
-
-        foreach (var file in files)
-        {
-            List<string> arguments =
-            [
-                "whisper",
-                $"\"{file.Path.LocalPath}\"",
-                $"--device {(ConfigService.Config.UseGpu ? "cuda" : "cpu")}",
-                $"-o {outputLocation}",
-                $"--output_format {ConfigService.Config.OutputFormat.ToString().ToLowerInvariant()}",
-                $"--model {ConfigService.Config.Model.ToString().ToLowerInvariant()}",
-                $"--language {language.TwoLetterISOLanguageName}"
-            ];
-
-            var info = new ProcessStartInfo
-            {
-                FileName = ConfigService.Config.PythonLocation,
-                Arguments = $"-m {string.Join(' ', arguments)}",
-                RedirectStandardOutput = true,
-                RedirectStandardError = true,
-                UseShellExecute = false,
-                CreateNoWindow = true
-            };
-            Console.WriteLine(info.Arguments);
-            var proc = new Process();
-            proc.StartInfo = info;
-            Console.WriteLine(1);
-
-            proc.OutputDataReceived += (a, b) => Console.WriteLine(a + " " + b.Data);
-            proc.ErrorDataReceived += (a, b) => Console.WriteLine("ERRRORF " + a + " " + b.Data);
-
-            Console.WriteLine(1.5);
-            proc.Start();
-            proc.BeginOutputReadLine();
-            proc.BeginErrorReadLine();
-            Console.WriteLine(2);
-
-            await proc.WaitForExitAsync();
-
-            if (proc.ExitCode != 0) throw new Exception("Python Exitcode: " + proc.ExitCode);
-        }
+        var settings = files.Select(f =>
+            new WhisperSettings(f.Path.LocalPath, outputLocation, language.TwoLetterISOLanguageName)).ToArray();
+        await WhisperService.Start(settings);
     }
 }
